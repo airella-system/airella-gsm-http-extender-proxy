a0e7c14e01db30e21c007c218aaffa910244ad23
Krzysztof PIaskowy <krzychu2956@gmail.com>
2020-09-23 23:23:39 +0200

    WORK

--- proxy.js
+++ proxy.js
@@ -1,68 +1,85 @@
-const http = require('http')
-const { hostname } = require('os')
+const https = require('https');
+const http = require('http');
 
-const requestListener = function (req, res) {
-  var data = ''
-  req.on('data', function(chunk) {
-    data += chunk
+const requestListener = function (request, response) {
+  var proxyData = ''
+
+  request.on('data', function(chunk) {
+    proxyData += chunk
   });
-  req.on('end', function() {
-    data = JSON.parse(data)
-    if(!valid(data)) {
-      res.writeHead(500)
-      res.end(JSON.stringify(
-        {
-          status: "fail",
-          message: "Incorrect data"
-        }
-      ))
+
+  request.on('end', function() {
+    proxyData = JSON.parse(proxyData)
+
+    if(!valid(proxyData)) {
+      makeProxyResponse(500, JSON.stringify({
+        status: "fail",
+        message: "Incorrect data"
+      }))
     } else {
-      let [hostname, path] = data['url'].split('.com/'); //todo
-      hostname += '.com'
-      
-      if(data['data'].lenght != 0) {
-        path += '?' + parseGetUrl(data['data'])
+      let httpType = checkHttpTypr(proxyData['url']);
+      let connector = httpType === 'http' ? http : https
+      let [hostname, port, path] = getUrlData(proxyData['url'], httpType)
+
+      let lowerMethod = proxyData['method'].toLowerCase()
+      if(lowerMethod === 'get') {
+        let separator = path.indexOf('?') == -1 ? '?' : ''
+        if(proxyData['data'] !== '') {
+          path += separator + parseGetUrl(proxyData['data'])
+        }
+      } else if(lowerMethod === "post" || lowerMethod === "put") {
+        proxyData['headers']['Content-Length'] = proxyData['data'].lenght
       }
 
-      let port = 80;
-      let portSeparaotrPosition = hostname.indexOf(':', 5); 
-      if(portSeparaotrPosition >= 0) {
-        port = hostname.substring(portSeparaotrPosition);
-      }
       let options = {
         hostname: hostname,
         port: port,
         path: path,
-        method: data['method'],
-        headers: data['headers']
+        method: proxyData['method'],
+        headers: proxyData['headers']
       }
-
-      let receivedData = '';
-      const proxyRequest = http.request(options, destResponse => {
-        console.log(`statusCode: ${destResponse.statusCode}`)
       
-        destResponse.on('data', d => {
-          receivedData = d
-          res.writeHead(200)
-          res.end(receivedData)
+      const proxyRequest = connector.request(options, destResponse => {
+        let receivedData = ''
+
+        destResponse.on('data', receivedChunk => {
+          receivedData += receivedChunk
         })
+
+        destResponse.on('end', function () {
+          makeProxyResponse(destResponse.statusCode, receivedData)
+        });
       })
+      .on('error', console.error)
+      .end(JSON.stringify(proxyData['data']))
       
       proxyRequest.on('error', error => {
         console.error(error)
       })
-      
-      proxyRequest.end()
-      
     }
-  });
+  })
+
+  response.setHeader('Content-Type', 'application/json')
+
+  function makeProxyResponse(code, data) {
+    response.writeHead(code)
+    response.end(data)
+  }
 }
 
 const server = http.createServer(requestListener);
 server.listen(8080);
 
 function valid(data) {
-  //todo: add validation
+  let requireProperty = ['method', 'url']
+  let optionalProperty  = ['headers', 'data']
+  let params = Object.keys(data)
+  for(let item of requireProperty) {
+    if(!params.includes(item)) return true;
+  }
+  for(let item of optionalProperty) {
+    if(!params.includes(item)) data[item] = "";
+  }
   return true;
 }
 
@@ -76,4 +93,36 @@ function parseGetUrl(args) {
     index++
   }
   return getParams
+}
+
+function checkHttpTypr(url) {
+  if(url.indexOf('http://') === 0) return "http";
+  if(url.indexOf('https://') === 0) return "https";
+  return "";
+}
+
+function getUrlData(url, httpType) {
+  let chunk
+  if(url.indexOf('http') === 0) {
+    chunk = url.substring(url.indexOf('/') + 2)
+  }
+
+  let hostname = ''
+  let path = ''
+  let port = httpType === 'http' ? 80 : 443
+  let slashPosition = chunk.indexOf('/')
+  if(slashPosition !== -1) {
+    hostname = chunk.substring(0, slashPosition)
+    path = chunk.substring(slashPosition)
+
+    let portPosition = hostname.indexOf(':')
+    if(portPosition!== -1){
+      port = hostname.substring(portPosition)
+      hostname = hostname.substring(0, portPosition)
+    }
+  } else {
+    hostname = chunk
+  }
+
+  return [hostname, port, path]
 }
